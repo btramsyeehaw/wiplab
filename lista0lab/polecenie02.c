19da74c7626c9b50368d87102d08159b87fce86f
btramsyeehaw <git>
2021-10-11 21:50:28 +0200

    lista 0 bez podpisów

---
+++ lista0lab/polecenie02.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+ float x;
+ float y;
+ float suma;
+ float roznica;
+ float iloczyn;
+ float iloraz;
+ scanf ("%f", &x);
+ scanf ("%f", &y);
+ suma = x+y;
+ printf("suma to %f \n", suma);
+ roznica = x-y;
+ printf("roznica to %f \n", roznica);
+ iloczyn = x*y;
+ printf("iloczyn to %f \n", iloczyn);
+ iloraz = x/y;
+ printf("iloraz to %f \n", iloraz);
+
+ return 0;
+}
---
+++ lista0lab/polecenie03.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+ float cel;
+ float fah;
+ printf("Wpisz temperature w celsjuszach:");
+ scanf("%f", &cel);
+ fah = 1.8 * cel + 32.0;
+ printf("Ta temperatura w fahrenheitach to: %f\n", fah);
+ return 0;
+}
